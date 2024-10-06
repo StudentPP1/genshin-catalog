d8ef8ea4a3d58692f3ec13b715d59d92e29d9fc0
StudentPP1 <mishamakytonin@gmail.com>
2024-10-07 01:06:11 +0300

    fix bug with loading images in catalog

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,57 +1,16 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import '../../styles/card.css'
 import { Link } from "react-router-dom";
 
-export const Card: React.FC<{ character: string }> = (props) => {
-  const [image, setImage] = useState<string>();
-  const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const getCharacterImg = async (character: string) => {
-      const url = `https://genshin.jmp.blue/characters/${character}/icon`;
-      const result = await fetch(url);
-      setImage(URL.createObjectURL(await result.blob())?.toString())
-    }
-
-    getCharacterImg(props.character).catch((e: Error) => {
-      setHttpError(e.message)
-      setIsLoading(false)
-    })
-    
-    setIsLoading(false)
-  }, [])
-
-  if (isLoading) {
-    return (
-        <div className="loader">
-            <div>
-            </div>
-            <div>
-            </div>
-            <div>
-            </div>
-            <div>
-            </div>
-        </div>
-    )
-  }
-
-  if (httpError) {
-    return (
-      <div>
-        {httpError}
-      </div>
-    )
-  }
+export const Card: React.FC<{ character: string , image: string}> = (props) => {
 
   function capitalize(text: string) {
     return text.charAt(0).toUpperCase() + text.slice(1);
   }
 
   return (
-    <Link to={`/${props.character}`} className="card rounded-lg text-center h-128 w-128 hover:scale-110">
-      <img loading="eager" className="rounded-t-lg" src={image} alt="icon" />
+    <Link to={`/${props.character}`} className="rounded-lg text-center h-128 w-128 hover:scale-110">
+      <img className="catalog-card rounded-t-lg" src={props.image} alt="icon" />
       <div className="card-title rounded-b-lg">
         {capitalize(props.character)}
       </div>
--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -6,37 +6,33 @@ function Catalog() {
     const [characterList, setCharacterList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [httpError, setHttpError] = useState<String | null>(null);
+    const [images, setImages] = useState<any>({});
 
     useEffect(() => {
         const getCharacterList = async () => {
             const url = "https://genshin.jmp.blue/characters";
             const result = await fetch(url);
             const json = await result.json();
-            setCharacterList(json)
+
+            const finish = json.map(async (character: string) => {
+                setIsLoading(true)
+                const url = `https://genshin.jmp.blue/characters/${character}/icon`;
+                const result = await fetch(url);
+                images[character] = URL.createObjectURL(await result.blob())?.toString()
+                setImages(images)
+            })
+    
+            Promise.all(finish).then(() => {
+                setCharacterList(json);
+                setIsLoading(false);
+            })
         }
 
         getCharacterList().catch((e: Error) => {
             setHttpError(e.message)
             setIsLoading(false)
         })
-        
-        setIsLoading(false)  
     }, [])
-
-    if (isLoading) {
-        return (
-            <div className="loader">
-                <div>
-                </div>
-                <div>
-                </div>
-                <div>
-                </div>
-                <div>
-                </div>
-            </div>
-        )
-    }
 
     if (httpError) {
         return (
@@ -46,20 +42,37 @@ function Catalog() {
         )
     }
     
+    if (isLoading) {
+        return (
+          <div className="loader">
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+            <div>
+            </div>
+          </div>
+        )
+    }
+      
     return (
         <>
-            <div className="mb-4"></div>
+        <div className="mb-4"></div>
             <h1 className='text-center font-whitney text-2xl'>
                 Characters
             </h1>
             <div className='py-6 px-4 sm:p-6 md:py-10 md:px-4'>
                 <div className='flex flex-wrap justify-center gap-5'>
                     {characterList.map((character) =>
-                        <Card character={character}/>
+                    <div>
+                        <Card character={character} image={images[character]}/>
+                    </div>
                     )}
                 </div>
             </div>
-        </>
+        </>    
     )
 }
 
